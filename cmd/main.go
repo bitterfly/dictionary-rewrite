85f8c918dc925859706e679698029d22c9ba3c09
bitterfly <dageneva@gmail.com>
2018-05-11 21:08:52 +0300

    working example from the paper

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,14 @@ import "local/ftransducer/transducer"
 import "fmt"
 
 func main() {
-	dict := map[string]string{"ab": "1", "abcd": "2", "d": "3"}
+	dict := map[string]string{"a": "1", "ab": "2", "abcc": "3", "babc":"4", "c":"5"}
 	t := transducer.NewTransducer([]rune{'a', 'b', 'c'}, dict)
 	t.Print()
 
-	fmt.Printf(fmt.Sprintf("%s\n", t.Replace([]rune("abd"))))
+	fmt.Printf(fmt.Sprintf("%s, %s\n", "abcbbbabccb", t.Replace([]rune("abcbbbabccb"))))
+	fmt.Printf(fmt.Sprintf("%s, %s\n", "a", t.Replace([]rune("a"))))
+	fmt.Printf(fmt.Sprintf("%s, %s\n", "ab", t.Replace([]rune("ab"))))
+	fmt.Printf(fmt.Sprintf("%s, %s\n", "abcc", t.Replace([]rune("abcc"))))
+	fmt.Printf(fmt.Sprintf("%s, %s\n", "babc", t.Replace([]rune("babc"))))
+	fmt.Printf(fmt.Sprintf("%s, %s\n", "c", t.Replace([]rune("c"))))
 }
